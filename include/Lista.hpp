bf4bb75b73b461db6a0acab8e91d64e8f63693b8
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-16 02:32:10 -0300

    Lista template com iterator funcionando (ate onde testei)

---
+++ include/Lista.hpp
@@ -0,0 +1,228 @@
+#pragma once
+
+namespace Listas
+{
+    template <class T>
+
+    class Lista
+    {
+    private:
+        template <class Type> 
+
+        class Elemento
+        {
+        private:
+            Type data;
+            Elemento<Type>* prox;
+            Elemento<Type>* ant;
+
+        public:
+            Elemento(Type dat, Elemento<Type>* anterior = nullptr, Elemento<Type>* prx = nullptr)
+                : data(dat), ant(anterior), prox(prx)
+            {}
+
+            Elemento()
+                : data(Type()), ant(nullptr), prox(nullptr)
+            {}
+
+            ~Elemento()
+            {
+                prox = nullptr;
+                ant = nullptr;
+            }
+
+            void setAnt(Elemento<Type>* anterior)
+            {
+                ant = anterior;
+            }
+            Elemento<Type>* getAnt() const
+            {
+                return ant;
+            }
+
+            void setProx(Elemento<Type>* prx)
+            {
+                prox = prx;
+            }
+            Elemento<Type>* getProx() const
+            {
+                return prox;
+            }
+
+            void setData(const Type dat)
+            {
+                data = dat;
+            }
+            const Type getData() const
+            {
+                return data;
+            }
+        };
+
+    private:
+        Elemento<T>* cabeca;
+        Elemento<T>* cauda;
+        int tam;
+
+    public:
+        class Iterator
+        {
+        private:
+            Elemento<T>* atual;
+
+        public:
+            Iterator(Elemento<T>* cabeca = nullptr)
+                : atual(cabeca)
+            {}
+
+            ~Iterator()
+            {
+                atual = nullptr;
+            }
+
+            T operator*()
+            {
+                return atual->getData();
+            }
+
+            Iterator& operator++()
+            {
+                if(atual)
+                    atual = atual->getProx();
+                return *this;
+            }
+
+            const bool operator==(const Iterator& outro) const
+            {
+                return atual == outro.atual;
+            }
+
+            const bool operator!=(const Iterator& outro) const
+            {
+                return atual != outro.atual;
+            }
+
+        public:
+            friend class Lista<T>;
+        };
+    
+    public:
+        Lista()
+            : cabeca(nullptr), cauda(nullptr), tam(0)
+        {}
+
+        ~Lista()
+        {
+            limpar();
+        }
+
+        void limpar()
+        {
+            while(cabeca)
+            {
+                Elemento<T>* temp = cabeca->getProx();
+                delete cabeca;
+                cabeca = temp;
+                --tam;
+            }
+            cabeca = nullptr;
+            cauda = nullptr;
+        }
+
+        void inserirNoFim(const T data)
+        {
+            if(!cabeca)    // Lista vazia
+            {
+                cauda = new Elemento<T>(data);
+                cabeca = cauda;
+                ++tam;
+                return;
+            }
+            Elemento<T>* temp = new Elemento<T>(data);
+            temp->setAnt(cauda);
+            cauda->setProx(temp);
+            cauda = temp;
+            ++tam;
+        }
+
+        void inserirNoInicio(const T data)
+        {
+            if(!cauda)    // Lista vazia
+            {
+                cabeca = new Elemento<T>(data);
+                cauda = cabeca;
+                ++tam;
+                return;
+            }
+            Elemento<T>* temp = new Elemento<T>(data);
+            temp->setProx(cabeca);
+            cabeca->setAnt(temp);
+            cabeca = temp;
+            ++tam;
+        }
+
+        void removerDoInicio()
+        {
+            if(!cauda)    // Lista vazia
+                return;
+            Elemento<T>* temp = cabeca->getProx();
+            if(temp)
+                temp->setAnt(nullptr);
+            else
+                cauda = nullptr;
+            delete cabeca;
+            cabeca = temp;
+            --tam;
+        }
+
+        void removerDoFim()
+        {
+            if(!cauda)    // Lista vazia
+                return;
+            Elemento<T>* temp = cauda->getAnt();
+            if(temp)
+                temp->setProx(nullptr);
+            else
+                cabeca = nullptr;
+            delete cauda;
+            cauda = temp;
+            --tam;
+        }
+
+        Iterator inicio()
+        {
+            return Iterator(cabeca);
+        }
+
+        Iterator fim()
+        {
+            return Iterator(nullptr);
+        }
+
+        const int getTam() const
+        {
+            return tam;
+        }
+
+        Iterator apagar(Iterator& it)
+        {
+            if(!cabeca || it == fim())
+                return fim();
+
+            Elemento<T>* temp = it.atual;
+
+            if(temp->getAnt())
+                temp->getAnt()->setProx(temp->getProx());
+            else
+                cabeca = temp->getProx();
+            if(temp->getProx())
+                temp->getProx()->setAnt(temp->getAnt());
+            else
+                cauda = temp->getAnt();
+            Iterator proxIt(temp->getProx());
+            delete temp;
+            --tam;
+            return proxIt;
+        }
+    };
+}
