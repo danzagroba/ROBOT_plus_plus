85558293f82cc3806e4713f6bd3ddded9313b09b
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-17 02:45:10 -0300

    inclusao de metodo para setar a posicao do sprite

--- include/Ente.hpp
+++ include/Ente.hpp
@@ -1,21 +1,24 @@
-#ifndef _ENTE_H_
-#define _ENTE_H_
-
-class Gerenciador_Grafico;
-class Figura;
+#pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+namespace Gerenciadores
+{
+    class Gerenciador_Grafico;
+}using namespace Gerenciadores;
 
 class Ente 
 {
     protected:
         int id;
-        static Gerenciador_Grafico* pGG;
-        Figura* pFig;
+        static Gerenciadores::Gerenciador_Grafico* pGG;
+        sf::Texture Textura;
+        sf::Sprite Figura;
     public:
         Ente();
-        ~Ente();
+        virtual ~Ente();
         virtual void executar() = 0;
-        void desenhar();
+        virtual void desenhar();
+        void setFigura(const std::string& Sprite_Path);
+        void setFiguraPos(const sf::Vector2f& pos);
+        const sf::Sprite& getFigura() const;
 };
-
-
-#endif
--- src/Ente.cpp
+++ src/Ente.cpp
@@ -1,17 +1,42 @@
 #include "Ente.hpp"
+#include <SFML/Graphics.hpp>
+
+#include "Gerenciador_Grafico.hpp"
+#include <string>
+
+
+
+Gerenciadores::Gerenciador_Grafico* Ente::pGG = Gerenciador_Grafico::getGerenciador_Grafico();
 
 Ente::Ente():
-id(-1),
-pFig(0)
+id(-1)
 {
 
 }
 
 Ente::~Ente()
 {
-    pFig = 0;
+    
 }
 void Ente::desenhar()
 {
+    pGG->desenharEnte(this);
+}
+void Ente::setFigura(const std::string& Sprite_Path)
+{
+
+    if (!Textura.loadFromFile(Sprite_Path))
+    {
+        cout<<"Erro no caminho da pasta do sprite, textura não carregada"<<endl;
+        return;
+    }
+    Figura.setTexture(Textura);
 
 }
+const sf::Sprite& Ente::getFigura() const{ return Figura; }
+void Ente::setFiguraPos(const sf::Vector2f& pos)
+{
+    Figura.setPosition(pos);
+}
+
+
