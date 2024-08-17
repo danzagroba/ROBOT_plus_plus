6296610b976449e82c524045d242d40cb25f8495
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-17 02:52:08 -0300

    inclusao de metodo para setar a posicao do sprite

--- include/Ente.hpp
+++ include/Ente.hpp
@@ -17,7 +17,7 @@ class Ente
         Ente();
         virtual ~Ente();
         virtual void executar() = 0;
-        virtual void desenhar();
+        void desenhar();
         void setFigura(const std::string& Sprite_Path);
         void setFiguraPos(const sf::Vector2f& pos);
         const sf::Sprite& getFigura() const;
