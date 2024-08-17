286642009268e652c203e00e1a5a23d7da031fc3
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-17 03:09:16 -0300

    função main atualizada

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,9 @@
-#include <SFML/Graphics.hpp>
+#include "Jogo.hpp"
 
-int main() {
-    // Create a window with the specified size and title
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
-
-    // Main loop to keep the window open
-    while (window.isOpen()) {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // Draw here (if needed)
-
-        // End the current frame
-        window.display();
-    }
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
 
     return 0;
 }
