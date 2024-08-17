0bded9659d674f3bad64fd8b28746d99864f3bb5
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-17 03:11:29 -0300

    construtor modificado

--- include/Personagem.hpp
+++ include/Personagem.hpp
@@ -2,18 +2,25 @@
 #define _PERSONAGEM_H_
 
 #include "Entidade.hpp"
-
-class Personagem: public Entidade 
+namespace Entidades
 {
-    protected:
-        int num_vidas;
-        int velocidade;
-    public:
-        Personagem();
-        ~Personagem();
-        void setNumVidas(const int nVidas);
-        virtual void executar() = 0;
-};
+    namespace Personagens 
+    {
+        class Personagem: public Entidade 
+        {
+            protected:
+                int num_vidas;
+                float velocidade;
+            public:
+                Personagem(const float vel = 0.0f, const int nVidas = 0, const sf::Vector2f& pos = sf::Vector2f(0.0f, 0.0f));
+                virtual ~Personagem();
+                void setNumVidas(const int nVidas);
+                const float getVel() const;
+                virtual void executar() = 0;
+                virtual void salvar() = 0;
+        };  
+    } using namespace Personagens;
+} using namespace Entidades;
 
 
 #endif
--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -1,8 +1,9 @@
 #include "Personagem.hpp"
 
-Personagem::Personagem():
-num_vidas(3),
-velocidade(0)
+Personagem::Personagem(const float vel, const int nVidas, const sf::Vector2f& pos):
+Entidade(pos),
+num_vidas(nVidas),
+velocidade(vel)
 {
 
 }
@@ -13,4 +14,8 @@ Personagem::~Personagem()
 void Personagem::setNumVidas(const int nVidas)
 {
     num_vidas = nVidas;
+}
+const float Personagem::getVel() const
+{
+    return velocidade;
 }
