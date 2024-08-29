7715f408447fadc8063fc49de55f04dcbb1de6a3
SmlYokoyama <samuelsy2005@gmail.com>
2024-08-29 18:34:55 -0300

    bandeira

---
+++ src/Bandeira.cpp
@@ -0,0 +1,35 @@
+#include "Bandeira.hpp"
+#include "Fase.hpp"
+
+namespace Entidades
+{
+    Bandeira::Bandeira(const sf::Vector2f& pos, Fases::Fase* pF, Jogador* pJU, Jogador* pJD)
+        : Entidade(pos, sf::Vector2f(0.0f, 0.0f)),
+          pFase(pF),
+          jogadorUm(pJU),
+          jogadorDois(pJD)
+    {
+        id = 40;
+        setFigura(BANDEIRA_SPRITE_PATH);
+    }
+
+    Bandeira::~Bandeira()
+    {}
+
+    void Bandeira::executar()
+    {
+        if(!pFase)
+            return;
+
+        if(jogadorUm)
+            if(getBoundingBox().intersects(jogadorUm->getBoundingBox()))
+                pFase->terminarFase();
+    
+        if(jogadorDois)
+            if(getBoundingBox().intersects(jogadorDois->getBoundingBox()))
+                pFase->terminarFase();
+    }
+
+    void Bandeira::salvar()
+    {}
+}
